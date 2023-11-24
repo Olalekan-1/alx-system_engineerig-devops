b847b4dcc5784927a7d1fc662710a2aa652ae045
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-11-24 23:12:57 +0100

    update solution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-# install flask with puppet
+# install flask version 2.1.0
+
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
-  command     => 'pip3 install Flask==2.1.0',
-  path        => '/usr/bin',
-  refreshonly => true,
-  require     => Package['python3-pip'],
+exec { 'install_flask_v2.1.0':
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
