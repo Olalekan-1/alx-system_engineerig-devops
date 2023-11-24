1f19176e06dc6fc5b095f51c7199cf61c4d38ace
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-11-24 23:52:34 +0100

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,38 @@
-# install flask version 2.1.0
+# install flask
 
-package { 'Werkzueg':
-  ensure   => '2.2.1'
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require => Package['python3-pip'],
 }
 
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
