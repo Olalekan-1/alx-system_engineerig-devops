4d3f574c56f76fb1d442500e0998f4f3489ae798
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-01-02 22:21:11 +0100

    add solution

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Gather data from a fake api and exports
+    to a csv file
+"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    employee_id = argv[1]
+    endpoint1 = f"https://jsonplaceholder.typicode.com/users?id={employee_id}"
+    endpoint2 = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+
+    name_response = requests.get(endpoint1).json()
+    employee_name = name_response[0]['name']
+
+    tasks_response = requests.get(endpoint2).json()
+    all_data = []
+    file_name = f"{employee_id}.csv"
+
+    for dict_ in tasks_response:
+        row_data = []
+        row_data.append(str(employee_id))
+        row_data.append(employee_name)
+        row_data.append(dict_['completed'])
+        row_data.append(dict_['title'])
+        all_data.append(row_data)
+
+    with open(file_name, mode='w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerows(all_data)
