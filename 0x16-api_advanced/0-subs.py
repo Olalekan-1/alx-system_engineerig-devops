6d9877d61f9d5c374f21dcbbf14773536c28648a
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-01-17 00:25:37 +0100

    add solution

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Make a request to the reddit api"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            return data['data']['subscribers']
+        elif response.status_code == 404:
+            return 0
+        else:
+            print(f"Error: {response.status_code}")
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
