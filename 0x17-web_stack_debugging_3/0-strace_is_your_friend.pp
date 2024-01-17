aeadbbf1ed998526501200988c05be7cd68bc0f6
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-01-17 23:55:07 +0100

    add solution

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'fix-wordpress-erro':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path => '/usr/local/bin/:/bin/',
+    command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
     }
